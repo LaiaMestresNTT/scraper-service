9ec9a5cb59fd82a691e56927c31d41ca8a3d0d3a
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2025-12-30 11:37:38 +0100

    pruebas para pipeline 2

--- src/main/java/com/alertbot/scraperservice/ScraperServiceApplication.java
+++ src/main/java/com/alertbot/scraperservice/ScraperServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScraperServiceApplication {
     public static void main(String[] args) {
-        System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         SSLUtil.disableCertificateValidation();
         System.out.println("ADVERTENCIA: Validación SSL/TLS deshabilitada.");
 
